cb51f6b12f240b92c19f48dabcb1e92f5a911632
Rune Mostad <rune.mostad@ntebb.no>
2016-01-24 13:49:07 -0800

    Legger til overlse3

---
+++ ov3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import telnetlib
+
+TELNET_PORT = 23
+TELNET_TIMEOUT = 6
+
+def main():
+    ip_addr = '50.76.53.27'
+    username = 'pyclass'
+    password = '88newclass'	
+ 
+    remote_conn = telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
+    remote_conn.close
+
+if __name__ == "__main__":
+    main()
+ 
